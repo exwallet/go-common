462115d1e2fefc304b90f11ed2a1db05b654edcc
kidd <chaseey@gmail.com>
2019-12-31 17:51:11 +0800

    add locker

---
+++ onceLocker/locker.go
@@ -0,0 +1,48 @@
+/*
+ * @Author: kidd
+ * @Date: 2019/12/31 下午5:46
+ */
+
+package onceLocker
+
+import (
+	"github.com/exwallet/go-common/log"
+	"sync"
+)
+
+// 占用锁
+type OnceLocker struct {
+	isLock bool
+	*sync.Mutex
+}
+
+func NewLocker() *OnceLocker {
+	return &OnceLocker{
+		isLock: false,
+		Mutex:  new(sync.Mutex),
+	}
+}
+
+func (l *OnceLocker) IsLock() bool {
+	return l.isLock
+}
+
+func (l *OnceLocker) Lock() {
+	if l.isLock {
+		log.Error("already locked")
+		return
+	}
+	l.Mutex.Lock()
+	l.isLock = true
+}
+
+func (l *OnceLocker) Unlock() {
+	if l.isLock {
+		l.Mutex.Unlock()
+		l.isLock = false
+		return
+	}
+	log.Error("not lock yet")
+}
+
+
---
+++ onceLocker/locker_test.go
@@ -0,0 +1,36 @@
+/*
+ * @Author: kidd
+ * @Date: 2019/12/31 下午5:47
+ */
+
+package onceLocker
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+
+func Test_1(t *testing.T) {
+	l1 := NewLocker()
+	fmt.Println("l1 before lock", l1.isLock)
+	l1.Lock()
+	fmt.Println("l1 after lock ", l1.isLock)
+	l1.Lock()
+	l1.Unlock()
+	fmt.Println("l1 after unlock ", l1.isLock)
+
+	l2 := NewLocker()
+	fmt.Println("l2 before lock", l2.isLock)
+	l2.Lock()
+	fmt.Println("l2 after lock ", l2.isLock)
+	l2.Lock()
+	l2.Unlock()
+	fmt.Println("l2 after unlock ", l2.isLock)
+
+
+
+
+}
+
